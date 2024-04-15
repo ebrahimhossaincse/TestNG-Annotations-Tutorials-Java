f2149f97579d56d1d17373a96d6acbd6a0e09d47
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-04-15 13:02:48 -0400

    Add New Annotation

--- src/test/java/ebrahim/hossain/sqa/IgnoreExample.java
+++ src/test/java/ebrahim/hossain/sqa/IgnoreExample.java
@@ -2,7 +2,8 @@ package ebrahim.hossain.sqa;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -11,19 +12,17 @@ public class IgnoreExample {
 	protected static String url = "https://www.testingtherapy.com/";
 	WebDriver driver;
 
-	@BeforeMethod
+	@BeforeSuite
 	public void startChromeBrowser() {
-		System.out.println("Before Method: This will run before each test method.");
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 	}
 
-	@Test
+	@Ignore
 	public void firstTest() throws InterruptedException {
 		driver.get(url);
 		Thread.sleep(3000);
-		driver.close();
 	}
 
 	@Test
