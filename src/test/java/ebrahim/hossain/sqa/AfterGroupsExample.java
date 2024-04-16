8df50d50ec827b13ee4c086289cd499fc26133e6
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-04-16 03:09:19 -0400

    add aftergroups annotation

--- src/test/java/ebrahim/hossain/sqa/AfterGroupsExample.java
+++ src/test/java/ebrahim/hossain/sqa/AfterGroupsExample.java
@@ -8,35 +8,33 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AfterGroupsExample {
+	WebDriver driver;
 
-    protected static String url = "";
-    WebDriver driver;
+	@BeforeGroups(groups = { "webTests" })
+	public void startChromeBrowser() {
+		System.out.println("Before Groups: This will run before any test in the 'webTests' group.");
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
 
-    @BeforeGroups(groups = {"webTests"})
-    public void startChromeBrowser() {
-        System.out.println("Before Groups: This will run before any test in the 'webTests' group.");
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
+	@Test(groups = "webTests")
+	public void firstTest() throws InterruptedException {
+		driver.get("https://www.testingtherapy.com/");
+		Thread.sleep(3000);
+	}
 
-    @Test(groups = "webTests")
-    public void firstTest() throws InterruptedException {
-        driver.get("https://www.testingtherapy.com/");
-        Thread.sleep(3000);
-    }
+	@Test(groups = "webTests")
+	public void secondTest() throws InterruptedException {
+		driver.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php");
+		Thread.sleep(3000);
+	}
 
-    @Test(groups = "webTests")
-    public void secondTest() throws InterruptedException {
-        driver.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php");
-        Thread.sleep(3000);
-    }
-
-    @AfterGroups(groups = {"webTests"})
-    public void tearDown() {
-        System.out.println("After Groups: This will run after all tests in the 'webTests' group.");
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+	@AfterGroups(groups = { "webTests" })
+	public void tearDown() {
+		System.out.println("After Groups: This will run after all tests in the 'webTests' group.");
+		if (driver != null) {
+			driver.quit();
+		}
+	}
 }
