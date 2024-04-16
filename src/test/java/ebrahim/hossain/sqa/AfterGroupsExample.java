2b2e527f396286b1799cfd2e19e6ecc3191d3b57
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-04-16 03:08:57 -0400

    add aftergroups annotation

---
+++ src/test/java/ebrahim/hossain/sqa/AfterGroupsExample.java
@@ -0,0 +1,42 @@
+package ebrahim.hossain.sqa;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterGroups;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AfterGroupsExample {
+
+    protected static String url = "";
+    WebDriver driver;
+
+    @BeforeGroups(groups = {"webTests"})
+    public void startChromeBrowser() {
+        System.out.println("Before Groups: This will run before any test in the 'webTests' group.");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test(groups = "webTests")
+    public void firstTest() throws InterruptedException {
+        driver.get("https://www.testingtherapy.com/");
+        Thread.sleep(3000);
+    }
+
+    @Test(groups = "webTests")
+    public void secondTest() throws InterruptedException {
+        driver.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php");
+        Thread.sleep(3000);
+    }
+
+    @AfterGroups(groups = {"webTests"})
+    public void tearDown() {
+        System.out.println("After Groups: This will run after all tests in the 'webTests' group.");
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
