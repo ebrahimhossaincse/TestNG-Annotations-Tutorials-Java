a506ac73977a81bc5a4ec87595512520d0e6b460
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-04-16 03:05:04 -0400

    add before groups

---
+++ src/test/java/ebrahim/hossain/sqa/BeforeGroupsExample.java
@@ -0,0 +1,44 @@
+package ebrahim.hossain.sqa;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BeforeGroupsExample {
+
+    protected static String url = "";
+    WebDriver driver;
+
+    @BeforeGroups(groups = {"webTests"})
+    public void startChromeBrowser() {
+        System.out.println("Before Groups: This will run before any test in the 'webTests' group.");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test(groups = "webTests")
+    public void firstTest() throws InterruptedException {
+        driver.get("https://www.testingtherapy.com/");
+        Thread.sleep(3000);
+//        driver.close();
+    }
+
+    @Test(groups = "webTests")
+    public void secondTest() throws InterruptedException {
+        driver.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php");
+        Thread.sleep(3000);
+//        driver.close();
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
